72daa79435a73e33a154604c7f713b2653203bcc
John Thornton <jthornton@gnipsel.com>
2020-01-10 17:38:30 -0600

    fix init and config

--- expgen/expgen/__init__.py
+++ expgen/expgen/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""Main entry point for expressions.
+"""Main entry point for expgen.
 
 This module contains the code necessary to be able to launch QControl
 directly from the command line, without using qtpyvcp. It handles
@@ -8,9 +8,9 @@ parsing command line args and starting the main application.
 
 Example:
     Assuming the dir this file is located in is on the PATH, you can
-    launch expressions by saying::
+    launch expgen by saying::
 
-        $ expressions --ini=/path/to/config.ini [options ...]
+        $ expgen --ini=/path/to/config.ini [options ...]
 
     Run with the --help option to print a full list of options.
 
@@ -29,8 +29,8 @@ def main(opts=None):
 
     if opts is None:
         from qtpyvcp.utilities.opt_parser import parse_opts
-        opts = parse_opts(vcp_cmd='expressions',
-                          vcp_name='expressions',
+        opts = parse_opts(vcp_cmd='expgen',
+                          vcp_name='expgen',
                           vcp_version=__version__)
 
     qtpyvcp.run_vcp(opts, VCP_CONFIG_FILE)
